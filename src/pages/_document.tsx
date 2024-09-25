577cc9f91ac7aa493b188d68a9786ca6556153c9
CristianLopes <cristianqibins@hotmail.com>
2024-09-25 19:05:27 -0300

    add home ui

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,34 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { getCssText } from '@/styles'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='anonymous'
+        />
+
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <title>Ignite Shop</title>
+
+        <link
+          href='https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,700;1,700&family=Roboto:wght@400;700&display=swap'
+          rel='stylesheet'
+        />
+
+        <style
+          id='stitches'
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
